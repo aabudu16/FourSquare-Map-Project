a8bb7e5f9cb4ce43f2851d39cd9e0ccd58f532dd
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-04 14:12:48 -0500

    Initial commit

---
+++ FourSquare-Map_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquare-Map_Project
+//
+//  Created by Mr Wonderful on 11/4/19.
+//  Copyright © 2019 Mr Wonderful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
