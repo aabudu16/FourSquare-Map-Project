93d1fb2d217d275a5618562169d35ca68af55f58
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-11-06 17:05:35 -0500

    Adds Constraints

--- View/ResultListTableViewCell.swift
+++ View/ResultListTableViewCell.swift
@@ -12,23 +12,27 @@ class ResultListTableViewCell: UITableViewCell {
     
     lazy var categoryImage:UIImageView = {
         let image = UIImageView()
-        image.backgroundColor = .blue
+        image.image = #imageLiteral(resourceName: "imagePlaceholder")
         return image
     }()
       
     lazy var storeLabel:UILabel = {
     let label = UILabel()
+        label.text = "Store name"
         return label
     }()
     
     lazy var categoryLabel:UILabel = {
     let label = UILabel()
+        label.text = "Category name"
         return label
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style:style, reuseIdentifier: reuseIdentifier)
         configureImageViewConstraints()
+        configureStoreLabelConstraints()
+        configureCategoryLabelConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -48,11 +52,15 @@ class ResultListTableViewCell: UITableViewCell {
     
     private func configureStoreLabelConstraints(){
     self.addSubview(storeLabel)
-    categoryImage.translatesAutoresizingMaskIntoConstraints = false
+    storeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([storeLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),storeLabel.leadingAnchor.constraint(equalTo: self.categoryImage.trailingAnchor, constant: 10), storeLabel.heightAnchor.constraint(equalToConstant: 30), storeLabel.widthAnchor.constraint(equalToConstant: 200)])
     }
 
     private func configureCategoryLabelConstraints(){
        self.addSubview(categoryLabel)
        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([categoryLabel.topAnchor.constraint(equalTo: storeLabel.bottomAnchor, constant: 10), categoryLabel.leadingAnchor.constraint(equalTo: self.categoryImage.trailingAnchor, constant: 10), categoryLabel.heightAnchor.constraint(equalTo: self.storeLabel.heightAnchor), categoryLabel.widthAnchor.constraint(equalTo: self.storeLabel.widthAnchor)])
        }
 }
